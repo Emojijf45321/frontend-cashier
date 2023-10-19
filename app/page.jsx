a10792a760f0de377b7ae5ddc834d92d0fbce50e
Emojijf45321 <abdulkholikalbachri28@gmail.com>
2023-10-19 08:10:35 +0700

    update sistem kategori

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>HomePage
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam earum illum commodi porro esse quaerat neque, perferendis aliquam enim optio asperiores, voluptas omnis eum excepturi suscipit ad, quas labore modi.</p>
+    </div>
+  )
+}
+
+export default HomePage
